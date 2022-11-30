a3f87b5be9c8be01b474662cbe07d6ee277931d7
nabillashinta <111963237+nabillashinta@users.noreply.github.com>
2022-11-30 19:34:47 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main() {
+    cout << "Nama: Nabilla Shinta Zahra"<< endl;
+    cout << "class: X PPLG 1"<< endl;
+    cout << "=========="<< endl;
+    cout << "Angka1 = 10"<< endl;
+    cout << "Angka2 = 5"<< endl;
+    cout << "Hasil:"<< 10+5 << endl;
+return 0;
+}
